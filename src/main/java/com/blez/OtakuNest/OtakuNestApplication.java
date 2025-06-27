754984873e887e567514865a4670f52b85f5d06f
blezDev <msaikrishnapatnaik@gmail.com>
2025-06-28 00:31:26 +0530

    setup

---
+++ src/main/java/com/blez/OtakuNest/OtakuNestApplication.java
@@ -0,0 +1,13 @@
+package com.blez.OtakuNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtakuNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtakuNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blez/demo/OtakuNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blez.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtakuNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
